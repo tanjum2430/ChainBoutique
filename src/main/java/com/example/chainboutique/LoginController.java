6297196d1f667c1355548c0601382d7f933fbaa4
adib69-ux <adibhasan69@gmail.com>
2026-08-05 20:26:58 +0600

    Initial project upload

---
+++ src/main/java/com/example/chainboutique/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.chainboutique;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField userIdField;
+    @javafx.fxml.FXML
+    private Button loginButton;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOnClic(ActionEvent actionEvent) {
+    }
+}
